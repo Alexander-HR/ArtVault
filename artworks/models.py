7dfc5516035fd8d1bdcef050ab0e2e3e40e8d9ca
alexiav24 <alexiav24@ru.is>
2026-05-05 09:43:57 +0000

    Model classes for Artwork and Bid

--- artworks/models.py
+++ artworks/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+class Artwork(models.Model):
+    MEDIUM_CHOICES = [
+        ('oil', 'Oil painting'),
+        ('water', 'Watercolour Painting'),
+        ('sculpture', 'Sculpture'),
+        ('photo', 'Photography'),
+        ('other', 'Other')
+    ]
+    STYLE_CHOICES = [
+        ('abstract', 'Abstract'),
+        ('realism', 'Realism'),
+        ('impressionism', 'Impressionism'),
+        ('modern', 'Modern'),
+        ('other', 'Other')
+    ]
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200)
+    medium = models.CharField(choices=MEDIUM_CHOICES, max_length=50)
+    style = models.CharField(choices=STYLE_CHOICES, max_length=50)
+    starting_bid = models.PositiveIntegerField(default=0)
+    date_listed = models.DateField(auto_now=False, auto_now_add=True)
+    sold = models.BooleanField(default=False)
+    seller = models.ForeignKey(User, on_delete=models.CASCADE)
+    dimensions = models.CharField(max_length=50)
+    year_created = models.PositiveIntegerField()
+    edition = models.CharField(max_length=50)
+    provenance = models.TextField()
+    def __str__(self):
+        return self.title
+
+class ArtworkImage(models.Model):
+    id = models.AutoField(primary_key=True)
+    artwork = models.ForeignKey(Artwork, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='artwork/%Y/%m')
+    def __str__(self):
+        return f'Image for {self.artwork.title}'
