80bef01a87b69c64da2f1168c66decea8d1210ad
dianabrk230525 <dianabrk2006@gmail.com>
2026-05-07 22:06:48 +0000

    Use AUTH_USER_MODEL in Profile model

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     image = models.ImageField(upload_to="profile_images/", blank=True, null=True)
 
     def __str__(self):
