4ea687b05480000cccfed031f65b98654905a219
alexiav24 <alexiav24@ru.is>
2026-05-09 21:41:10 +0000

    Added filtering, searching and sorting

--- artworks/views.py
+++ artworks/views.py
@@ -16,4 +16,37 @@ def artwork_detail(request, artwork_id):
 
 def index(request):
     artworks = Artwork.objects.all()
-    return render(request, 'artworks/index.html', {'artworks': artworks})
+
+    search = request.GET.get("search")
+    medium = request.GET.get("medium")
+    style = request.GET.get("style")
+    order_by = request.GET.get("order_by")
+    sold = request.GET.get("sold")
+
+    if search:
+        artworks = artworks.filter(title__icontains=search)
+
+    if medium:
+        artworks = artworks.filter(medium=medium)
+
+    if style:
+        artworks = artworks.filter(style=style)
+
+    if order_by in ["starting_bid", "-starting_bid", "title", "-title"]:
+        artworks = artworks.order_by(order_by)
+
+    if sold == "sold":
+        artworks = artworks.filter(sold=True)
+
+    elif sold == "available":
+        artworks = artworks.filter(sold=False)
+
+    return render(request, "artworks/index.html", {
+        "artworks": artworks,
+        "medium_choices": Artwork.MEDIUM_CHOICES,
+        "style_choices": Artwork.STYLE_CHOICES,
+        "selected_medium": medium,
+        "selected_style": style,
+        "search": search,
+        "order_by": order_by,
+        "selected_sold": sold,})
