70c4064803e4ebfebde305ce734cdf1d7bfe9c58
Alexander-HR <alexanderje24@ru.is>
2026-05-10 22:10:48 +0000

    Signup now works

--- accounts/views.py
+++ accounts/views.py
@@ -1,19 +1,18 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-from .forms import ProfileForm
+from .forms import ProfileForm, CustomUserCreationForm
 from .models import Profile
-from django.contrib.auth.forms import UserCreationForm
 
 
 def signup(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect("login")
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, "accounts/signup.html", {"form": form})
 
 @login_required
