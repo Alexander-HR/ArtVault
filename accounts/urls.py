1176b5656a95ccd033e8280db32e248d782a3af8
jjing890701-sys <jjing890701@gmail.com>
2026-05-11 20:52:52 +0000

    Add create seller profile flow

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,13 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
-from .views import profile_view, seller_profile, seller_list
 from . import views
 
 urlpatterns = [
     path('signup', views.signup, name='signup'),
     path('login', LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout', LogoutView.as_view(), name='logout'),
-    path("profile/", profile_view, name="profile"),
+    path("profile/", views.profile_view, name="profile"),
+    path("create-seller-profile/", views.create_seller_profile, name="create_seller_profile"),
     path("sellers/", views.seller_list, name="sellers"),
-    path("sellers/<int:seller_id>/", seller_profile, name="seller_profile"),
+    path("sellers/<int:seller_id>/", views.seller_profile, name="seller_profile"),
 ]
