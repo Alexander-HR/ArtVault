9ae391518ef170e52c2657766214523d50f845f0
jjing890701-sys <jjing890701@gmail.com>
2026-05-11 23:25:15 +0000

    Add footer pages

--- artvault/urls.py
+++ artvault/urls.py
@@ -6,6 +6,8 @@ import artworks
 from artvault import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -13,7 +15,11 @@ urlpatterns = [
     path("", include("accounts.urls")),
     path("artworks/", include("artworks.urls")),
     path("bids/", include("bids.urls")),
-    
+    path("about/", TemplateView.as_view(template_name="pages/about.html"), name="about"),
+    path("contact/", TemplateView.as_view(template_name="pages/contact.html"), name="contact"),
+    path("help/", TemplateView.as_view(template_name="pages/help.html"), name="help"),
+    path("privacy/", TemplateView.as_view(template_name="pages/privacy.html"), name="privacy"),
+
 ]
 
 if settings.DEBUG:
