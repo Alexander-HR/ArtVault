1ed9806d1e5970e935a59db9137d59575edaf87b
alexiav24 <alexiav24@ru.is>
2026-05-14 09:22:54 +0000

    added Message model to admin site

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,8 +4,10 @@ from accounts.models import Profile
 from accounts.models import User
 from accounts.models import Seller
 from accounts.models import Address
+from accounts.models import Message
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Seller)
 admin.site.register(Profile)
 admin.site.register(Address)
+admin.site.register(Message)
